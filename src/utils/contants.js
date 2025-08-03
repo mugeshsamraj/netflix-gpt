aa5c3a482d56cae379ce228f7557b79190553019
Revanth SR <143567879+Revangapeth2003@users.noreply.github.com>
2025-08-03 13:52:34 +0530

    changes

---
+++ src/utils/contants.js
@@ -0,0 +1,15 @@
+export const Login_BG ='https://assets.nflxext.com/ffe/siteui/vlv3/7968847f-3da9-44b3-8bbb-13a46579881f/web/IN-en-20250609-TRIFECTA-perspective_32b70b51-20d4-46db-8a1a-3d5428be5f0e_large.jpg'
+
+export const Login_Logo = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDcxZDI1OTAzMjA0OWRkNTA2YjY2MGRlOTg2YjU3YiIsIm5iZiI6MTc1MDQ2NDE1Mi4zODYsInN1YiI6IjY4NTVmNjk4NDFmMmU0NzQyZGE3OGYyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0ZQCkjqJCUdfkisyCpNPEavy18m97nw8Tf2-dieAJWA'
+  }
+};
+
+export const Image_CDN = `https://image.tmdb.org/t/p/w500`
+
+export const SUPPORTED_LANGUAGE = [{identifier:'en',name:"English"},{identifier:'ta',name:"Tamil"},{identifier:'sp',name:"Spanish"}]
